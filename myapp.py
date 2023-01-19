8f083e7d69dcfd1c910a9317a8cbfb12ad687cf6
N-Hi <120696131+N-Hi@users.noreply.github.com>
2023-01-19 17:42:33 +0000

    1st

---
+++ myapp.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+
+def load_data():
+    iris = datasets.load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df['target'] = iris.target_names[iris.target]
+    return df
+
+df = load_data()
+targets = list(df.target.unique())
+selected_targets = st.multiselect('select targets', targets, default=targets)
+df = df[df.target.isin(selected_targets)]
+
+st.dataframe(df)
