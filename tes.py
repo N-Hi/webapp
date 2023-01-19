06bc1f75bd2a925c3901fe7284cbcbad8009628f
N-Hi <120696131+N-Hi@users.noreply.github.com>
2023-01-19 15:30:32 +0000

    1st

--- tes.py
+++ tes.py
@@ -1,1 +1,4 @@
-ss
+import statsmodels.api as sm
+import pandas as pd
+import plotly.express as px
+import plotly
--- web_newapp.py
+++ web_newapp.py
@@ -1,41 +1,69 @@
+import time
 import streamlit as st
-import networkx as nx
-from mypulp import GRB, Model, quicksum, multidict, tuplelist
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import pandas as pd
+import plotly.express as px
+import plotly
+from statsmodels.tsa.api import Holt
 
 
-model=Model("lo1")
+#タイトル
+st.title("機械学習アプリ")
+st.write("streamlitで実装")
 
-#ユーザーからの入力を取得
-formula1 = st.text_input("数式1")
-formula2 = st.text_input("数式2")
-formula3 = st.text_input("数式3")
-formula4 = st.text_input("数式4")
+# 以下をサイドバーに表示
+st.sidebar.markdown("### 機械学習に用いるcsvファイルを入力してください")
 
-formulaX = st.text_input("求めたいもの（式でも可）")
+#ファイルアップロード
+uploaded_files = st.sidebar.file_uploader("Choose a CSV file", accept_multiple_files= False)
 
-#最大、最少を求め、結果出す
-model.addConstr(formula1)
-model.addConstr(formula2)
-model.addConstr(formula3)
-model.addConstr(formula4)
+#ファイルがアップロードされたら以下が実行される
+if uploaded_files:
+    df = pd.read_csv(uploaded_files)
+    df_columns = df.columns
 
-#最大値
-model.setObjective(formulaX,GRB.MAXIMIZE)
+    #データフレームを表示
+    st.markdown("### 入力データ")
+    st.dataframe(df.style.highlight_max(axis=0))
 
-model.optimize()
+    #matplotlibで可視化。X軸,Y軸を選択できる
+    st.markdown("### 可視化 単変量")
 
-result_MAX = model.ObjVal
-st.write("最大値:")
-st.write("result_MAX")
+    #データフレームのカラムを選択オプションに設定する
+    x = st.selectbox("X軸", df_columns)
+    y = st.selectbox("Y軸", df_columns)
 
-#最小値
-model.setObjective(formulaX,GRB.MINIMIZE)
+    #選択した変数を用いてmtplotlibで可視化
+    fig = plt.figure(figsize= (12,8))
+    plt.scatter(df[x],df[y])
+    plt.xlabel(x,fontsize=18)
+    plt.ylabel(y,fontsize=18)
+    st.pyplot(fig)
 
-model.optimize()
+    st.markdown("### モデリング")
+    #説明変数は複数選択式
+    ex = st.multiselect("日付の列を選択してください", df_columns)
 
-result_MIN = model.ObjVal
-st.write("最小値:")
-st.write("result_MIN")
+    #目的変数は一つ
+    ob = st.selectbox("目的変数を選択してください", df_columns)
+
+    st.markdown("#### 機械学習を実行します")
+    execute = st.button("実行")
+
+    df["ex"]=pd.to_datetime(df.Date)
+    df.set_index("Date",inplace=True)
+    df.index.frep="MS"
+
+    fit=Holt(df[:]).fit(smoothing_level=0.8,smoothing_slope=0.2,optimized=False)
+    start, end =len(df)-24,len(df)
+    predict2=fit.predict(start, end)
+
+    predict2.plot()
+
+
 
 
 
